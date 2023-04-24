8e31fdef3187101ec8c45160b02dbdc7dbdf7cd0
TSCOMjvilla <jvilla@tscom.cl>
2023-04-24 17:53:06 -0400

    ingreso de usuario y migraciones

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,13 +16,21 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
-        //obtener campo en particular
-        //dd($request->get('email'));
         $this->validate($request,[
             'name'=>'required|min:3|max:30',
             'username'=>'required|unique:users|min:3|max:20',
             'email'=>'unique:users|email|max:60',
-            'password'=> 'required'
+            'password'=> 'required|confirmed|min:6'
         ]);
+
+        User::create([
+            'name'=>$request->name,
+            'username'=> Str::slug($request->username),
+            'email'=>$request->email,
+            'password'=> Hash::make($request->password) 
+        ]);
+
+        //redireccionamos al usuario
+
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -33,7 +33,8 @@
                         name="username"
                         type="text"
                         placeholder="Tu nombre de usuario"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
+                        value="{{old('username')}}"
                     >
                     @error('username')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
@@ -46,7 +47,8 @@
                         name="email"
                         type="email"
                         placeholder="Tu email de registro"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                        value="{{old('email')}}"
                     >
                     @error('email')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
@@ -59,7 +61,7 @@
                         name="password"
                         type="password"
                         placeholder="Password de registro"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
                     >
                     @error('password')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
@@ -72,7 +74,7 @@
                         name="password_confirmation"
                         type="password"
                         placeholder="Repite tu password"
-                        class="border p-3 w-full rounded-lg"
+                        class="border p-3 w-full rounded-lg @error('password_confirmation') border-red-500 @enderror"
                     >
                 </div>
                 <input type="submit" 
